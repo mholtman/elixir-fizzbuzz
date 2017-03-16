5f4633b55c44356fec9de7c24985650c8002729d
Mark Holtman <mholtman@gmail.com>
2017-03-16 15:37:06 -0400

    Refactored and cleaned up using conventions

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,7 +1,12 @@
 defmodule FizzBuzz do
   @moduledoc """
-  Documentation for FizzBuzz.
+  Documentation for FizzBuzz
   """
+
+  defp __processNumber({0, 0, _}), do: "fizzbuzz"
+  defp __processNumber({0, _, _}), do: "fizz"
+  defp __processNumber({_, 0, _}), do: "buzz"
+  defp __processNumber({_, _, number}), do: number
 
   @doc """
   FizzBuzz
@@ -13,12 +18,7 @@ defmodule FizzBuzz do
 
   """
 
-  defp processNumber({0, 0, _}), do: "fizzbuzz"
-  defp processNumber({0, _, _}), do: "fizz"
-  defp processNumber({_, 0, _}), do: "buzz"
-  defp processNumber({_, _, number}), do: number
-
-
-  def fizzbuzz(num), do: processNumber({ rem(num, 3), rem(num, 5), num })
+  @spec fizzbuzz(integer) :: String.t
+  def fizzbuzz(num), do: __processNumber({rem(num, 3), rem(num, 5), num})
 
 end
